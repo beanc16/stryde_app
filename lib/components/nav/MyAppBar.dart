06e52e7c267fa80c75d08d8dd99b50131ceb9bcd
beanc16 <Bean.Christopher@outlook.com>
2021-03-20 20:00:51 -0400

    Update AppBar to contain an SVG of Stryde's logo.

--- lib/components/nav/MyAppBar.dart
+++ lib/components/nav/MyAppBar.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:workout_buddy/components/misc/StrydeColors.dart';
-import 'package:workout_buddy/utilities/NavigatorHelpers.dart';
-import 'package:workout_buddy/utilities/UiHelpers.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class MyAppBar
@@ -19,12 +18,61 @@ class MyAppBar
       titleText = Text(title);
 
       myAppBarNoButton = AppBar(
-        title: titleText,
+        //leading: getLogo(8),
+        title: getLogoTitleRow(),
+        //title: getLogo(8),
         backgroundColor: StrydeColors.lightBlue,
       );
     }
 
     return myAppBarNoButton;
+  }
+
+
+
+  static Widget getLogoTitleRow()
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        getLogo(8),
+        titleText
+      ],
+    );
+  }
+
+  static Widget getLogo(double padding)
+  {
+    Widget svg = SvgPicture.asset(
+      "images/strydeLogoClean.svg",
+      semanticsLabel: "Stryde Logo",
+
+      fit: BoxFit.contain,
+      height: 40,
+
+      placeholderBuilder: (BuildContext context) =>
+          _getLogoPadding(context, padding),
+    );
+
+    return Padding(
+      child: svg,
+      padding: EdgeInsets.only(
+        top: padding,
+        bottom: padding,
+        right: padding,
+      ),
+    );
+  }
+
+  static Widget _getLogoPadding(BuildContext context, double padding)
+  {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: padding,
+        top: padding,
+        bottom: padding,
+      ),
+    );
   }
 
 
@@ -35,5 +83,7 @@ class MyAppBar
     {
       titleText = Text(newTitle);
     }
+
+    // Else, set appbar's state
   }
 }
